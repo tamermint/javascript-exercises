61070e1b0239e412b58c98cb32f4b6a306a070f4
Vivek <vivek.mitra@outlook.com>
2023-02-21 20:16:13 +1100

    fixed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (typeof(a) != "number" || typeof(b) != "number") {
+        return 'ERROR';
+    }
+    else if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    else if (b < a) {
+        for (let i = b; i <= a; i++) {
+           sum += i;
+        }
+    }
+    else {
+        for(let i = a; i <= b; i++)
+            {
+                sum += i;
+            }
+    }
+    return sum;
 };
 
 // Do not edit below this line
