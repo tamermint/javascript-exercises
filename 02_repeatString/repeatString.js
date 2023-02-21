97e678d5b16f86ac292f6c59355021967c369c3d
Vivek <vivek.mitra@outlook.com>
2023-02-21 18:43:18 +1100

    fixed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newStr = "";
+    if(num === 0) {
+        return '';
+    }
+    else if(num < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            newStr += str;
+        }
+    }
+    return newStr;
 };
 
 // Do not edit below this line
