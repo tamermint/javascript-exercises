273f9cf955aa40d1d1e7ae37de0670fd0702d711
Vivek <vivek.mitra@outlook.com>
2023-04-08 10:23:40 +1000

    Exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a) + (b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return (a) - (b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  }
+  else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
