5f5bd7ac111b191a949a35c385e9012eea22ec8e
Vivek <vivek.mitra@outlook.com>
2023-04-08 10:38:19 +1000

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return processedStr.split("").reverse().join("") == processedStr;
 };
 
 // Do not edit below this line
