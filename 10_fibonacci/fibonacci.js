b73ed080c62875a8f91359467d6d3b0d299377e6
Vivek <vivek.mitra@outlook.com>
2023-04-08 11:47:28 +1000

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+ //make an empty array
+    const fibArray = []; //for generating initial array till n + 1
+ if (n <= 0) {
+   return "OOPS";
+ }
+ else if (n != Number) {
+    n = parseInt(n);
+ }
+ fibArray.push(0, 1);
+ for (i = 2; i < n + 1; i++) {
+   const nextNum = fibArray[i - 1] + fibArray[i - 2];
+   fibArray.push(nextNum);
+ }
+ return fibArray[n];
+}
 
-};
+ //if n is negative return "OOPS"
+ //else if n is a string or a number , convert it using ParseInt
+    //return number present at n index
+
 
 // Do not edit below this line
 module.exports = fibonacci;
